a925cac566105ae2f88dccbee75e91656b347f30
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2025-06-28 19:48:20 +0200

    tuple data structure

---
+++ Python/tuples.py
@@ -0,0 +1,13 @@
+my_tuple =(1, 2, 3, 4, 5)
+print(my_tuple[0])
+print(my_tuple[1])
+print(my_tuple[2])
+print(my_tuple[-1])
+
+my_tuple1 = (4, 5, 6)
+my_tuple2 = (7, 8, 9)
+my_tuple3 = my_tuple1 + my_tuple2
+print(my_tuple3)
+
+rep_myttuple = my_tuple3 * 2
+print(rep_myttuple)
