d00ec07c031f437c59356a14e9aceedfb685ac43
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2025-06-28 19:16:38 +0200

    completed the calculator app, started and completed python lists

---
+++ Python/lists.py
@@ -0,0 +1,18 @@
+
+fruits = ["Banana", "Apple", "Cherry"]
+print(fruits)
+
+fruits[2] = "Grapes"
+print(fruits)
+
+fruits.insert(1,"Strawberry")
+print(fruits)
+
+fruits.append("Pineapple")
+print(fruits)
+
+fruits.remove("Pineapple")
+print(fruits)
+
+fruits.sort()
+print(fruits)
