5cacca82253283844bc7b54e88ea12f2b533186c
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2025-06-28 22:31:25 +0200

    dictionory data structure practice.

---
+++ Python/dictionary.py
@@ -0,0 +1,10 @@
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+print(my_dict)
+
+#adding a new key-value pair
+my_dict['d'] = 4
+print(my_dict)
+
+#modifying an existing key-value pair
+my_dict['c'] = 5
+print(my_dict)
