b6a60ba0f0af45e3a9c03314a2dfe17a70491f86
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2025-06-28 22:55:09 +0200

    python operators plus another assignment

---
+++ Python/operators.py
@@ -0,0 +1,31 @@
+#Addition(+)
+#Subtraction(-)
+#Multiplication(*)
+#Division(/)
+#Exponentiation(**)
+#Modulo(%)
+
+x = 10
+y = 5
+
+# Addition
+print(f"Addition: {x + y}")
+# Subtraction
+print(f"Subtraction: {x - y}")
+# Multiplication
+print(f"Multiplication: {x * y}")
+# Division
+print(f"Division: {x / y}")
+# Exponentiation
+print(f"Exponentiation: {x ** y}")
+# Modulo
+print(f"Modulo: {x % y}")
+
+# assignment operators.adding a value to a variable that aleady has a value.
+
+x += 2
+print(f"After adding 2: {x}")
+
+# Subtracting a value from a variable that already has a value.
+x -= 3
+print(f"After subtracting 3: {x}")
