9ec6c59029cee1ae6319cf0bda194425414cd2d3
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2025-06-29 10:08:50 +0200

    Learning about variables, valid and invalid variable names

--- Python/Week7/numericData.py
+++ Python/Week7/numericData.py
@@ -1,3 +1,4 @@
+'''
 #Numeric Data
 num = 7
 print(num)
@@ -6,3 +7,16 @@ print(type(num))
 num_float = 7.5
 print(num_float)
 print(type(num_float))
+'''
+
+#variables
+#Variables are used to store data in a program.
+#Valid variable names can contain letters, numbers, and underscores, but cannot start with a number.
+# Variable names are case-sensitive, meaning that `myVariable` and `myvariable` are considered different variables.
+my_variable = 10
+my_variable2 = 20
+print(my_variable)
+print(my_variable2)
+# You can perform arithmetic operations with variables
+sum_variable = my_variable + my_variable2
+print(f"Sum of my_variable and my_variable2: {sum_variable}")
