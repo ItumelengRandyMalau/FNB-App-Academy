f1dde6d028e577cdd918be728b16e958318e8a8d
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2025-06-29 10:27:41 +0200

    operators on strings

---
+++ Python/Week7/operators_on_strings.py
@@ -0,0 +1,9 @@
+#Operators on strings
+string1 = "Hello"
+string2 = "World"
+# Concatenation
+concatenated_string = string1 + " " + string2
+print("Concatenated String:", concatenated_string)
+# Repetition
+repeated_string = string1 * 3
+print("Repeated String:", repeated_string)
