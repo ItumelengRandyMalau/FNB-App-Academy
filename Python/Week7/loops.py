e3ad47718332085019753480b01a42bda66ca51b
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2025-06-30 17:48:16 +0200

    for loop and a while loop.

---
+++ Python/Week7/loops.py
@@ -0,0 +1,22 @@
+'''
+#for loop
+
+import numbers
+
+
+fruits = ["Banana", "Apple", "Cherry"]
+for fruit in fruits:
+    print(fruit)
+
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    print(number)
+'''
+
+#While loop
+
+count = 0
+
+while count <= 7:
+    print(count)
+    count += 1  # Increment count by 1 each iteration
