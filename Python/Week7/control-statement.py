eddd6403025943a895bd6cdb6385da984296a472
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2025-06-30 17:24:20 +0200

    Control statement continuation.

--- Python/Week7/control-statement.py
+++ Python/Week7/control-statement.py
@@ -1,5 +1,6 @@
 #Control Statements
 
+'''
 num = 7
 
 if num > 0:
@@ -8,3 +9,15 @@ elif num == 0:
     print("Number is zero")
 else:
     print("Number is negative")
+'''
+
+num1 = int(input("Enter the first number "))
+num2 = int(input("Enter the second number "))
+
+if num1 > num2:
+    print("num1 is greater than num2 ")
+
+elif num2 < num1:
+    print("num2 is less than num1 ")
+else:
+    print("Both numbers are equal")
