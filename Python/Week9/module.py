eb4b2083263c65bbbf4a611be593c3d5584c0a9f
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2025-07-01 18:04:06 +0200

    examples of using python built-in modules

---
+++ Python/Week9/module.py
@@ -0,0 +1,4 @@
+import math_operations
+
+print(math_operations.add(5, 3))  # Output: 8
+print(math_operations.subtract(10, 4))  # Output: 6
---
+++ Python/Week9/modules_builtin.py
@@ -0,0 +1,12 @@
+import math
+import random
+import datetime
+import os
+import sys
+
+# Example usage of built-in modules
+print(sys.version)  # Output: Python version
+print(os.getcwd())  # Output: Current working directory
+print(datetime.datetime.now())  # Output: Current date and time
+print(random.randint(1, 10))  # Output: Random integer between 1 and 10
+print(math.pi)
